19c87a97fbbc97dd1ce67c87c0d874bfefa7817e
jamiemai <maicuong1701@gmail.com>
2021-07-16 08:59:05 +0700

    restore-file-theme

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,3 +1,2 @@
 export { default as Image } from './Image';
 export { default as Icon } from './Icon';
-export { default as LearnMoreLink } from './LearnMoreLink';
--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,5 +1,2 @@
 export { default as Section } from './Section';
-export { default as NightSection } from './NightSection';
-export { default as SectionAlternate } from './SectionAlternate';
-export { default as CardBase } from './CardBase';
-export { default as CardPricingStandard } from './CardPricingStandard';
+
