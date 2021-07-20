e1fc11f7d30dc1b8a11faa0bee2d58155e2afc56
jamiemai <maicuong1701@gmail.com>
2021-07-20 15:32:10 +0700

    fixed oder

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
