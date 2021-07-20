1a7ab82c565703422d11a00f5615ad0448728020
jamiemai <maicuong1701@gmail.com>
2021-07-20 16:05:09 +0700

    update aws

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+// import Amplify, { Auth } from 'aws-amplify';
+// import awsconfig from './aws-exports';
 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
